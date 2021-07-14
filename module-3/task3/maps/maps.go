b9527765100e07cc0203538b0e5940bb2a849c8d
Anton Ilchenko <anton_ilchenko@epam.com>
2021-07-14 14:55:07 +0200

    FEAT: Module3.Task3 solved

--- module-3/task3/maps/maps.go
+++ module-3/task3/maps/maps.go
@@ -1,4 +1,22 @@
 package maps
 
+import (
+	"sort"
+	"strings"
+)
+
 func SortedValues(m map[int]string) string {
+	keys := make([]int, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+
+	sort.Ints(keys)
+
+	vals := make([]string, 0)
+	for _, v := range keys {
+		vals = append(vals, m[v])
+	}
+
+	return strings.Join(vals[:], "")
 }
