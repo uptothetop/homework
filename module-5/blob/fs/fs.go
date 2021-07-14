2074ca81943494239b5fba84fa73b551f52809f0
Anton Ilchenko <anton_ilchenko@epam.com>
2021-07-14 17:07:52 +0200

    FEAT: Module5.Task1 Completed

--- module-5/blob/fs/fs.go
+++ module-5/blob/fs/fs.go
@@ -1,13 +1,77 @@
 package blobfs
 
-func Get(key string) ([]byte, string) {
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"os"
+)
 
+const filename = "storage.json"
+
+var storage = make(map[string][]byte)
+
+func Get(key string) ([]byte, error) {
+	loadStorage()
+	val, err := storage[key]
+	if err {
+		return nil, errors.New("error getting data from the storage")
+	}
+	return val, nil
 }
 
-func Put(key string, value []byte) string {
+func Put(key string, value []byte) error {
+	var err error
+	defer func() {
+		if result := recover(); result != nil {
+			err = errors.New("error storing value")
+		}
+	}()
 
+	storage[key] = value
+	saveStorage()
+	return err
 }
 
-func Delete(key string) string {
+func Delete(key string) error {
+	loadStorage()
+	var err error
+	defer func() {
+		if result := recover(); result != nil {
+			err = errors.New("error storing value")
+		}
+	}()
 
+	delete(storage, key)
+	saveStorage()
+	return err
+}
+
+// Loads storage from the FS
+func loadStorage() error {
+	var err error
+
+	jsonFile, err := os.Open("users.json")
+
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	json.Unmarshal(byteValue, &storage)
+
+	return err
+}
+
+// Saves storage to the FS
+func saveStorage() error {
+	var err error
+
+	jsonData, err := json.Marshal(storage)
+	jsonFile, err := os.Create(filename)
+
+	defer jsonFile.Close()
+
+	jsonFile.Write(jsonData)
+	jsonFile.Close()
+
+	return err
 }
--- module-5/blob/memory/memory.go
+++ module-5/blob/memory/memory.go
@@ -1,18 +1,37 @@
 package blolbmemory
 
-// Exported to use it from FS blob
-var Storage = make(map[string][]byte)
+import "errors"
+
+var storage = make(map[string][]byte)
 
 func Get(key string) ([]byte, error) {
-	return Storage[key], nil // TODO: Return errors
+	val, err := storage[key]
+	if err {
+		return nil, errors.New("error getting data from the storage")
+	}
+	return val, nil
 }
 
 func Put(key string, value []byte) error {
-	Storage[key] = value
-	return nil // TODO: Return errors
+	var err error
+	defer func() {
+		if result := recover(); result != nil {
+			err = errors.New("error storing value")
+		}
+	}()
+
+	storage[key] = value
+	return err
 }
 
 func Delete(key string) error {
-	delete(Storage, key)
-	return nil // TODO: Return errors
+	var err error
+	defer func() {
+		if result := recover(); result != nil {
+			err = errors.New("error storing value")
+		}
+	}()
+
+	delete(storage, key)
+	return err
 }
