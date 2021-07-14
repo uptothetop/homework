242af7aaa29b3767b7133640e7e792ef2afbfb7a
Anton Ilchenko <anton_ilchenko@epam.com>
2021-07-14 16:28:14 +0200

    FEAT: Implemented memory blob storage

--- module-5/blob/memory/memory.go
+++ module-5/blob/memory/memory.go
@@ -1,18 +1,18 @@
 package blolbmemory
 
-type Storage struct {
-	key   string
-	value []byte
+// Exported to use it from FS blob
+var Storage = make(map[string][]byte)
+
+func Get(key string) ([]byte, error) {
+	return Storage[key], nil // TODO: Return errors
 }
 
-func Get(key string) ([]byte, string) {
-
+func Put(key string, value []byte) error {
+	Storage[key] = value
+	return nil // TODO: Return errors
 }
 
-func Put(key string, value []byte) string {
-
-}
-
-func Delete(key string) string {
-
+func Delete(key string) error {
+	delete(Storage, key)
+	return nil // TODO: Return errors
 }
